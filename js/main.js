11574a2ed7e9dd866c79c28bcad242208b228b5f
Rob O'Donnell <r.odonnell@farmlead.com>
2017-03-05 14:37:40 -0500

    added link locations

--- js/main.js
+++ js/main.js
@@ -7,17 +7,18 @@ var $dataExp = $('[data-category*=experience]');
 var $dataPhoto = $('[data-category*=photo]');
 var $dataIll = $('[data-category*=illustration]');
 var $dataMotion = $('[data-category*=motion]');
-var $filterBrand = $('("#filter-select").val("filter-branding")');
 
-$filterBrand.onClick(function(){
-    $dataWeb.css("opacity", ".4");
-    $dataPrint.css("opacity", ".4");
-    $dataType.css("opacity", ".4");
-    $dataUx.css("opacity", ".4");
-    $dataExp.css("opacity", ".4");
-    $dataPhoto.css("opacity", ".4");
-    $dataIll.css("opacity", ".4");
-    $dataMotion.css("opacity", ".4");
+var $filterBrand = $('("#filter-select").val("filter-branding");');
+
+$filterBrand.click(function() {
+  $dataWeb.css("opacity", ".4");
+  $dataPrint.css("opacity", ".4");
+  $dataType.css("opacity", ".4");
+  $dataUx.css("opacity", ".4");
+  $dataExp.css("opacity", ".4");
+  $dataPhoto.css("opacity", ".4");
+  $dataIll.css("opacity", ".4");
+  $dataMotion.css("opacity", ".4");
 });
 
 function openNav() {
